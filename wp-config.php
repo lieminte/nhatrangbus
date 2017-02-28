c0e5ab56784673dd67040a432a44622290eaaae6
18 10 15 PL <18 10 15 PL>
2017-02-28 08:47:09 +0700

    F

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'hocwordpress');
+define('DB_NAME', 'u293341390_dbbus');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'u293341390_admin');
 
 /** MySQL database password */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', 'ngocliem95');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
