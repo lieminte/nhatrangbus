fdad0057a9359ddff71fbc5ed68021b52df04b86
18 10 15 PL <18 10 15 PL>
2017-02-28 09:01:36 +0700

    090118022017

--- wp-config.php
+++ wp-config.php
@@ -29,7 +29,7 @@ define('DB_USER', 'u293341390_admin');
 define('DB_PASSWORD', 'ngocliem95');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'mysql.hostinger.vn');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8mb4');
